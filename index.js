fb87f44d10541d12980b4c79c4ee58bb5669f347
Abubaker <137734278+AbubakerSalah@users.noreply.github.com>
2024-03-31 08:06:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const ColorBtn = document.getElementById("color-btn")
+const colorInput = document.getElementById("color")
+const dropdownOptions = document.getElementById("dropdown-options")
+const colorColumns = document.getElementById("color-columns")
+const hexId = document.getElementById("hex-id")
+const counter = document.getElementById("counter")
+
+ColorBtn.addEventListener("click", function(e) {
+    e.preventDefault()
+
+    const colorInputValue = colorInput.value.slice(1)
+    const optionSelectValue = dropdownOptions.value
+    const counterValue = counter.value
+
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorInputValue}&mode=${optionSelectValue}&count=${counterValue}`)
+        .then(response => response.json())
+        .then(data => {
+            let colorHtml = ""
+            let hexValueHTML = ""
+
+            data.colors.map(color => {
+                colorHtml += `<div class ="color-scheme" style="background-color: ${color.hex.value}"></div>`
+                hexValueHTML += `<p>${color.hex.value}</p>`
+            })
+
+            colorColumns.innerHTML = colorHtml
+            hexId.innerHTML = hexValueHTML
+            
+        })
+        
+})
